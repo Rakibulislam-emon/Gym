825b11445e6d5348288bcea71db30272a2fd5c09
Rakibulislam-emon <rakibulislamemon60>
2024-07-11 03:32:29 +0600

    test

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const navItems = [
 ]
   return (
     <div>
-      <div className="top-0 py-1 lg:py-2 w-full bg-transparent lg:relative z-50 dark:bg-gray-900">
+      <div className="top-0 py-1 lg:py-2 w-full  lg:relative z-50 dark:bg-gray-900">
         <nav className="z-10 sticky top-0 left-0 right-0 max-w-4xl xl:max-w-5xl mx-auto px-5 py-2.5 lg:border-none lg:py-4">
           <div className="flex items-center justify-between">
             <button>
